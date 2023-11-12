7b6f9948bc8a508a0edede766b547cf8cfb571f1
Vktr12104 <116500975+Vktr12104@users.noreply.github.com>
2023-11-12 16:08:57 +0700

    console

---
+++ Source/console.h
@@ -0,0 +1,15 @@
+#include "boolean.h"
+#include "ADT/ADT_List/listdinamis.h"
+#include "ADT/ADT_List/liststatis.h"
+#include "ADT/ADT_Map/"
+#include "ADT/ADT_Mesin-Kata/mesinkata.h"
+#include "ADT/ADT_MesinKarakter/mesinkarakter.h"
+#include "ADT/ADT_Queue/circular_queue.h"
+#include "ADT/ADT_Queue/queue.h"
+#include "ADT/ADT_Set/set.h"
+#include "ADT/ADT_Stack/stack.h"
+
+
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
