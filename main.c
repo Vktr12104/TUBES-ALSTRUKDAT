5c28404469d638e3eb43c193bfe3ee406bc241e5
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-15 19:46:26 +0700

    update main

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include "Source/ADT/ADT_Map/map.h"
 #include "Source/ADT/ADT_List/liststatis.h"
 #include "Source/start.h"
+#include "Source/ADT/ADT_Set/set.h"
 
 #include <stdio.h>
 
 int main(){
     char namaFile[] = "text.txt";
-    ListPenyanyi listmusisi2 = MakeListPenyanyi();
-    STARTREAD(&listmusisi2,namaFile);
-    DisplayListPenyanyi(listmusisi2);
+    ListPenyanyi listmusisi3;
+    ListLagu lagu2;
+    STARTREAD(&listmusisi3,&lagu2,namaFile);
+    DisplayListPenyanyi(listmusisi3);
+    DisplayListLagu(lagu2,4);
 
     
 
