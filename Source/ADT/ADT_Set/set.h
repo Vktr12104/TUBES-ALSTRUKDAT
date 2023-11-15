032ca1be2c87fdae3249d710e0913c5190ba25c4
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-15 19:48:30 +0700

    update set untuk ListLagu

--- Source/ADT/ADT_Set/set.h
+++ Source/ADT/ADT_Set/set.h
@@ -2,7 +2,9 @@
 #define set_H
 #include <stdio.h>
 #include "boolean.h"
+#include "../ADT_Mesin-Kata/mesinkata.h"
 # include "../struc.h"
+
 
 /* Definisi Set S kosong : S.Count = Nil */
 /* S.Count = jumlah element Set */
@@ -11,27 +13,26 @@
 /* ********* Prototype ********* */
 
 /* *** Konstruktor/Kreator *** */
-void CreateEmptyLagu(Lagu *S);
+void CreateEmptyListLagu(ListLagu *S);
 /* I.S. Sembarang */
 /* F.S. Membuat sebuah Set S kosong berkapasitas MaxEl */
 
 
 /* ********* Predikat Untuk test keadaan KOLEKSI ********* */
 
-boolean IsEmptyLagu(Lagu S);
+boolean IsEmptyListLagu(ListLagu W);
 /* Mengirim true jika Set S kosong*/
 
-boolean IsFullLagu(Lagu S);
-/* Mengirim true jika Lagu S penuh */
-
-
 /* ********** Operator Dasar Lagu ********* */
-void InsertLagu(Lagu *S, Title Elmt);
+void InsertListLagu(ListLagu *S, Word Elmt,int idalbum);
 /* Menambahkan Elmt sebagai elemen Set S. */
 /* I.S. S mungkin kosong, S tidak penuh
         S mungkin sudah beranggotakan Elmt */
 /* F.S. Elmt menjadi anggota dari S. Jika Elmt sudah merupakan anggota, operasi tidak dilakukan */
 
-boolean IsMemberLagu(Lagu S, Title Elmt);
+boolean IsMemberLagu(ListLagu S, Word Elmt);
+/* True jika Elmt sudah member dari ListLagu*/
+
+void DisplayListLagu(ListLagu S, int idalbum);
 
 #endif
