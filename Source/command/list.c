36259e69be10141e23c1f0996cb2cf1c348960bb
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-18 18:42:22 +0700

    update command list default (udah bener harusnya)

--- Source/command/list.c
+++ Source/command/list.c
@@ -3,51 +3,41 @@
 #include "../ADT/struc.h"
 
 
-void listdefault(ListPenyanyi input1, SetLagu input3 /*, MapAlbum input2*/){
+void listdefault(ListPenyanyi input1, SetLagu input3 , MapAlbum input2){
     printf("Daftar Penyanyi :\n");
-    for (int i = 0;i<input1.NEff;i++){
-        printf("%d. %s\n",i+1,input1.A[i]);
-    }
+    DisplayListPenyanyi(input1);
     printf("Ingin melihat album yang ada?(Y/N) ");
     STARTCOMMAND();
     if (currentCommand.TabWord[0]=='Y'){
         printf("Pilih penyanyi untuk melihat album mereka: \n");
         STARTCOMMAND();
-        char *comm = wordToString(currentCommand);
-        // note : bisa langsung for kalo mark semicolon = currentCommand g ada semicolon
-        // for (int i = 0;i<input.penyanyi_length;i++){
-        //     if (StrComp(comm,input.penyanyi[i].nama_penyanyi)){
-        //         thealbum = input.penyanyi[i];
-        //         break;
-        //         } 
-        // }
-        // for (int i = 0;i<thealbum.album_length;i++){
-        //     printf("%d. %s\n", i+1, thealbum.album_penyanyi[i].nama_album);
-        // }
+        Word kata = currentCommand;
+        printf("Daftar album oleh ");
+        displayWord(kata);
+        printf(" : \n");
+        int indexpenyanyi = albumtoidpenyanyi(input1,kata);
+        displayMapAlbum(input2,indexpenyanyi);
         int idpenyanyi;
-        for (int i = 0;i<=input1.NEff;i++){
+       /* for (int i = 0;i<=input1.NEff;i++){
             char* namap = wordToString(input1.A[i]);
             if (StrComp(comm,namap)){
                 idpenyanyi = i+1;
                 break;
             }
-        }
+        }*/
         SetLagu thelagu;
         printf("Ingin melihat lagu yang ada?(Y/N) ");
         STARTCOMMAND();
         if (currentCommand.TabWord[0]=='Y'){
             printf("Pilih album untuk melihat lagu yang ada di album: \n");
             STARTCOMMAND();
-            char *comm = wordToString(currentCommand);
-            int idalbum = 1; 
-            int id = 1;
-            for (int i =0;i<input3.jumlah_lagu;i++){
-                if (input3.A[i].album_id==idalbum){
-                    Word lagu = input3.A[i].nama_lagu;
-                    printf("%d. %s\n",id,lagu.TabWord);
-                    id++;
-                }
-            }
+            Word cur = currentCommand;
+            int id = laguAlbumID(cur,input2);
+            printf("Daftar lagu di ");
+            displayWord(cur);
+            printf(" :\n");
+            DisplaySetLagu(input3,id);
+            //DisplaySetLagu(input3,id);
         }
     }
 }
--- Source/command/list.h
+++ Source/command/list.h
@@ -1,11 +1,13 @@
 #ifndef __comlist__
 #define __comlist__
 
-#include "boolean.h"
+#include "../ADT/boolean.h"
 #include "../ADT/struc.h"
 #include "../ADT/ADT_List/liststatis.h"
 #include "../ADT/ADT_Mesin-Kata/mesinkata.h"
+#include "../ADT/ADT_Set/set.h"
+#include "../ADT/ADT_Map/map.h"
 
-void listdefault(ListPenyanyi input1, SetLagu input3); /*, MapAlbum input2 */
+void listdefault(ListPenyanyi input1, SetLagu input3,MapAlbum input2); /*, MapAlbum input2 */
 void listplaylist(ListPlaylist input);
 #endif
