8105bc79b6e7b5d9f741fc9d61ed8aa63eff5dd7
Vktr12104 <116500975+Vktr12104@users.noreply.github.com>
2023-11-19 23:52:39 +0700

    demi listdinamis

--- Source/ADT/ADT_List/listdinamis.c
+++ Source/ADT/ADT_List/listdinamis.c
@@ -2,8 +2,8 @@
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor: create list kosong */
-List MakeList() {
-    List L;
+ListD MakeList() {
+    ListD L;
     L.A = (ListPlaylist*)malloc(MaxEl * sizeof(ListPlaylist));
     L.NEff = 0;
     L.Max = MaxEl;
@@ -12,75 +12,74 @@ List MakeList() {
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test list kosong *** */
-boolean IsEmpty(List L) {
+boolean IsEmpty(ListD L) {
     return L.NEff == 0;
 }
 
 /* ********** SELEKTOR ********** */
 /* *** Banyaknya elemen *** */
-int Length(List L) {
+int Length(ListD L) {
     return L.NEff;
 }
 
 /* *** Selektor INDEKS *** */
-IdxType FirstIdx(List L) {
+IdxType FirstIdx(ListD L) {
     return 0;
 }
 
-IdxType LastIdx(List L) {
+IdxType LastIdx(ListD L) {
     return L.NEff - 1;
 }
 
 /* ********** Test Indeks yang valid ********** */
-boolean IsIdxValid(List L, IdxType i) {
+boolean IsIdxValid(ListD L, IdxType i) {
     return (i >= 0) && (i < L.Max);
 }
 
-boolean IsIdxEff(List L, IdxType i) {
+boolean IsIdxEff(ListD L, IdxType i) {
     return (i >= 0) && (i < L.NEff);
 }
 
 /* ********** Operasi-operasi ********** */
-boolean IsMemberdinamis(List L, ListPlaylist X) {
+boolean IsMemberdinamis(ListD L, ListPlaylist X) {
     int i;
     for (i = 0; i < L.NEff; i++) {
-        if (StrComp(X.playlist->playlist_nama,L.A[i].playlist->playlist_nama)) {
+        if (StrComp(X.playlist->playlist_nama,L.A[i].NamaPlaylist)) {
             return true;
         }
     }
     return false;
 }
 
-void InsertFirst(List *L, ListPlaylist X) {
+void InsertFirst(ListD *L, Word nama) {
     if (L->NEff < L->Max) {
         int i;
         for (i = L->NEff; i > 0; i--) {
             L->A[i] = L->A[i - 1];
         }
-        L->A[0] = X;
+        *L->A[0].NamaPlaylist = nama;
         L->NEff++;
     }
 }
 
-void InsertAt(List *L, ListPlaylist X, IdxType i) {
+void InsertAt(ListD *L, Word nama, IdxType i) {
     if (L->NEff < L->Max && IsIdxEff(*L, i)) {
         int j;
         for (j = L->NEff; j > i; j--) {
             L->A[j] = L->A[j - 1];
         }
-        L->A[i] = X;
+        *L->A[i].NamaPlaylist=nama;
         L->NEff++;
     }
 }
-
-void InsertLast(List *L, ListPlaylist X) {
+void InsertLast(ListD *L, Word nama ) {
     if (L->NEff < L->Max) {
-        L->A[L->NEff] = X;
+        *L->A[L->NEff].NamaPlaylist = nama;
         L->NEff++;
     }
 }
 
-void DeleteFirst(List *L) {
+void DeleteFirst(ListD *L) {
     if (!IsEmpty(*L)) {
         int i;
         for (i = 0; i < L->NEff - 1; i++) {
@@ -90,7 +89,7 @@ void DeleteFirst(List *L) {
     }
 }
 
-void DeleteAt(List *L, IdxType i) {
+void DeleteAt(ListD *L, IdxType i) {
     if (!IsEmpty(*L) && IsIdxEff(*L, i)) {
         int j;
         for (j = i; j < L->NEff - 1; j++) {
