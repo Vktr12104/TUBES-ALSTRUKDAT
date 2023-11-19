6385c3926df95e87fe7cda427082ab5109d9a201
Vktr12104 <116500975+Vktr12104@users.noreply.github.com>
2023-11-19 23:52:55 +0700

    perbaiki sikit lah

--- Source/ADT/ADT_List/listdinamis.h
+++ Source/ADT/ADT_List/listdinamis.h
@@ -26,7 +26,7 @@ typedef struct {
 	int Max;
 } ListD;
 
-extern ListD list_dinamis
+extern ListD list_dinamis ;
 #define list(i) L.A[i]
 
 /* Indeks yang digunakan seberapa banyak memori itu terisi */
@@ -38,62 +38,62 @@ extern ListD list_dinamis
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor: create list kosong */
-List MakeList();
+ListD MakeList();
 /* I.S. sembarang */
 /* F.S. Terbentuk list L kosong dengan kapasitas MaxEl */
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test list kosong *** */
-boolean IsEmpty(List L);
+boolean IsEmpty(ListD L);
 /* Mengirimkan true jika list L kosong, mengirimkan false jika tidak */
 
 /* ********** SELEKTOR ********** */
 /* *** Banyaknya elemen *** */
-int Length(List L);
+int Length(ListD L);
 /* Mengirimkan banyaknya elemen efektif list */
 /* Mengirimkan nol jika list kosong */
 
 /* *** Selektor INDEKS *** */
-IdxType FirstIdx(List L);
+IdxType FirstIdx(ListD L);
 /* Prekondisi : list L tidak kosong */
 /* Mengirimkan indeks elemen pertama */
 
-IdxType LastIdx(List L);
+IdxType LastIdx(ListD L);
 /* Prekondisi : list L tidak kosong */
 /* Mengirimkan indeks elemen terakhir */
 
 /* ********** Test Indeks yang valid ********** */
-boolean IsIdxValid (List L, IdxType i);
+boolean IsIdxValid (ListD L, IdxType i);
 /* Prekondisi : i sembarang */
 /* Mengirimkan true jika i adalah indeks yang valid utk ukuran list */
 /* yaitu antara indeks yang terdefinisi untuk container*/
 
-boolean IsIdxEff (List L, IdxType i);
+boolean IsIdxEff (ListD L, IdxType i);
 /* Prekondisi : i sembarang*/
 /* Mengirimkan true jika i adalah indeks yang valid utk list */
 /* yaitu antara FirstIdx(L)..LastIdx(L) */
 
 /* ********** Operasi-operasi ********** */
-boolean IsMemberdinamis(List L, ListPlaylist X);
+boolean IsMemberdinamis(ListD L, ListPlaylist X);
 /* Prekondisi : X sembarang */
 /* Mengirimkan true jika terdapat elemen X di dalam list */
 /* yaitu antara FirstIdx(L)..LastIdx(L) */
 
-void InsertFirst(List *L, ListPlaylist X);
+void InsertFirst(ListD *L, Word nama);
 /* I.S. L terdefinisi, mungkin kosong. */
 /* F.S. v menjadi elemen pertama L. */
 
-void InsertAt(List *L, ListPlaylist X, IdxType i);
+void InsertAt(ListD *L, Word nama, IdxType i) ;
 /* I.S. L terdefinisi, tidak kosong, i merupakan indeks lojik yang valid di L. */
 /* F.S. v disisipkan dalam L pada indeks ke-i (bukan menimpa elemen di i). */
 
-void InsertLast(List *L, ListPlaylist X);
+void InsertLast(ListD *L, Word nama );
 /* I.S. L terdefinisi, mungkin kosong. */
 /* F.S. v menjadi elemen terakhir L. */
 
-void DeleteFirst(List *L);
+void DeleteFirst(ListD *L);
 /* I.S. L terdefinisi, tidak kosong. */
 /* F.S. F diset dengan elemen pertama L, elemen pertama L dihapus dari L. */
 
-void DeleteAt(List *L, IdxType i);
+void DeleteAt(ListD *L, IdxType i);
 #endif
