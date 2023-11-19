204c205d5bd5306c74aafa16d673543b2b97c9c7
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2023-11-19 12:16:50 +0700

    update linked list

--- Source/ADT/ADT_LinkedList/listb.h
+++ Source/ADT/ADT_LinkedList/listb.h
@@ -9,18 +9,19 @@
 #include "../ADT_Set/boolean.h"
 #include "../ADT_Mesin-Kata/mesinkata.h"
 #include <stdio.h>
+#include "ADT/struc.h"
 
 #define Nil NULL
 
 typedef char* infotype;
 typedef struct tElmtlist *address;
 typedef struct tElmtlist {
-    infotype info;
+    isi_playlist infoplaylist;
     address next;
-} ElmtList;
+} ElmtListBerkait;
 typedef struct {
     address First;
-} List;
+} listBerkait;
 
 /* Definisi list: */
 /* List kosong: First(L) = Nil */
@@ -32,11 +33,11 @@ typedef struct {
 
 /* PROTOTYPE */
 /****************** TEST LIST KOSONG ******************/
-boolean IsEmpty(List L);
+boolean IsEmpty(listBerkait L);
 /* Mengirim true jika list kosong */
 
 /****************** PEMBUATAN LIST KOSONG ******************/
-void CreateEmpty(List *L);
+void CreateEmpty(listBerkait *L);
 /* I.S. sembarang */
 /* F.S. Terbentuk list kosong */
 
@@ -52,100 +53,98 @@ void Dealokasi(address *P);
 /* Melakukan dealokasi/pengembalian address P */
 
 /****************** PENCARIAN SEBUAH ELEMEN LIST ******************/
-address Search(List L, infotype X);
+address Search(listBerkait L, infotype X);
 /* Mencari apakah ada elemen list dengan info(P) = X */
 /* Jika ada, mengirimkan address elemen tersebut. */
 /* Jika tidak ada, mengirimkan Nil */
 
 /****************** PRIMITIF BERDASARKAN NILAI ******************/
 /*** PENAMBAHAN ELEMEN ***/
-void InsVFirst(List *L, infotype X);
+void InsVFirst(listBerkait *L, infotype X);
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen pertama dengan nilai X jika alokasi berhasil */
-void InsVLast(List *L, infotype X);
+void InsVLast(listBerkait *L, infotype X);
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen list di akhir: elemen terakhir yang baru */
 /* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S. = F.S. */
 
 /*** PENGHAPUSAN ELEMEN ***/
-void DelVFirst(List *L, infotype *X);
+void DelVFirst(listBerkait *L, infotype *X);
 /* I.S. List L tidak kosong */
 /* F.S. Elemen pertama list dihapus: nilai info disimpan pada X */
 /* dan alamat elemen pertama di-dealokasi */
-void DelVLast(List *L, infotype *X);
+void DelVLast(listBerkait *L, infotype *X);
 /* I.S. list tidak kosong */
 /* F.S. Elemen terakhir list dihapus: nilai info disimpan pada X */
 /* dan alamat elemen terakhir di-dealokasi */
 
 /****************** PRIMITIF BERDASARKAN ALAMAT ******************/
 /*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
-void InsertFirst(List *L, address P);
+void InsertFirst(listBerkait *L, address P);
 /* I.S. Sembarang, P sudah dialokasi */
 /* F.S. Menambahkan elemen ber-address P sebagai elemen pertama */
-void InsertAfter(List *L, address P, address Prec);
+void InsertAfter(listBerkait *L, address P, address Prec);
 /* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
 /* P sudah dialokasi */
 /* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
-void InsertLast(List *L, address P);
+void InsertLast(listBerkait *L, address P);
 /* I.S. Sembarang, P sudah dialokasi */
 /* F.S. P ditambahkan sebagai elemen terakhir yang baru */
 
 /*** PENGHAPUSAN SEBUAH ELEMEN ***/
-void DelFirst(List *L, address *P);
+void DelFirst(listBerkait *L, address *P);
 /* I.S. List tidak kosong */
 /* F.S. P adalah alamat elemen pertama list sebelum penghapusan */
 /* Elemen list berkurang satu (mungkin menjadi kosong) */
 /* First element yg baru adalah suksesor elemen pertama yang lama */
-void DelP(List *L, infotype X);
+void DelP(listBerkait *L, infotype X);
 /* I.S. Sembarang */
 /* F.S. Jika ada elemen list ber-address P, dengan info(P) = X */
 /* Maka P dihapus dari list dan di-dealokasi */
 /* Jika tidak ada elemen list dengan info(P) = X, maka list tetap */
 /* List mungkin menjadi kosong karena penghapusan */
-void DelLast(List *L, address *P);
+void DelLast(listBerkait *L, address *P);
 /* I.S. List tidak kosong */
 /* F.S. P adalah alamat elemen terakhir list sebelum penghapusan */
 /* Elemen list berkurang satu (mungkin menjadi kosong) */
 /* Last element baru adalah predesesor elemen terakhir yg lama, */
 /* jika ada */
-void DelAfter(List *L, address *Pdel, address Prec);
+void DelAfter(listBerkait *L, address *Pdel, address Prec);
 /* I.S. List tidak kosong. Prec adalah anggota list */
 /* F.S. Menghapus Next(Prec): */
 /* Pdel adalah alamat elemen list yang dihapus */
 
 /****************** PROSES SEMUA ELEMEN LIST ******************/
-void PrintInfo(List L);
+void PrintInfo(listBerkait L);
 /* I.S. List mungkin kosong */
 /* F.S. Jika list tidak kosong, ia list dicetak ke kanan: [e1,e2,...,en] */
 /* Contoh: jika ada tiga elemen bernilai "one", "two", "three" akan dicetak: ["one","two","three"] */
 /* Jika list kosong: menulis [] */
 /* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
-int NbElmt(List L);
+int NbElmt(listBerkait L);
 /* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
 
 /*** Prekondisi untuk Max/Min/rata-rata: List tidak kosong ***/
-infotype Max(List L);
-/* Mengirimkan nilai info(P) yang maksimum */
-address AdrMax(List L);
+address AdrMax(listBerkait L);
 /* Mengirimkan address P, dengan info(P) yang bernilai maksimum */
-infotype Min(List L);
+infotype Min(listBerkait L);
 /* Mengirimkan nilai info(P) yang minimum */
-address AdrMin(List L);
+address AdrMin(listBerkait L);
 /* Mengirimkan address P, dengan info(P) yang bernilai minimum */
-float Average(List L);
+float Average(listBerkait L);
 /* Mengirimkan nilai rata-rata info(P) */
 
 /****************** PROSES TERHADAP LIST ******************/
 
-void InversList(List *L);
+void InversList(listBerkait *L);
 /* I.S. sembarang. */
 /* F.S. elemen list dibalik: */
 /* Elemen terakhir menjadi elemen pertama, dan seterusnya. */
 /* Membalik elemen list, tanpa melakukan alokasi/dealokasi. */
 
-void Konkat1(List *L1, List *L2, List *L3);
+void Konkat1(listBerkait *L1, listBerkait *L2, listBerkait *L3);
 /* I.S. L1 dan L2 sembarang */
 /* F.S. L1 dan L2 kosong, L3 adalah hasil konkatenasi L1 & L2 */
 /* Konkatenasi dua buah list: L1 dan L2 */
