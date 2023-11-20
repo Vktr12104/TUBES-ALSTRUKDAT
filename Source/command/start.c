891cc8ceb01eb9ca3be5ced5c97fac2d665e56ae
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-20 15:28:56 +0700

    update start.c fix

--- Source/command/start.c
+++ Source/command/start.c
@@ -20,37 +20,27 @@ void STARTREAD(ListPenyanyi * LP, SetLagu *lagu2,MapAlbum *m2,char NamaFile []){
   int album,lagu;
   int number =1;
   int idalbum=1;
-  printf("\nJumlah Penyanyi: %d\n", loop);
+  printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan\n");
   for (int i = 0; i < loop; i++){
     ADVSATUKATA();
     album = currentWord.TabWord[0] - 48;   
     ADVKALIMAT();  
-    displayWord(currentWord);
     printf("\n");
     InsertLast(&*LP, currentWord);
     for (int j = 0; j < album; j++){
       ADVSATUKATA();
       lagu = currentWord.TabWord[0] - 48;
-
       ADVKALIMAT();
-      displayWord(currentWord);
-      printf("\n");
       InsertMapAlbum(&*m2,i,currentWord);
-      //AddAlbum(LP, CLine);
-      //NamaAlbum = NamaAlbumNow(LP);
       for (int k = 0; k < lagu; k++)
       {
         ADVKALIMAT();
-        displayWord(currentWord);
-        printf("\n");
-        InsertSetLagu(&*lagu2,currentWord,number);
-        //AddLagu(LP, CLine);    
+        InsertSetLagu(&*lagu2,currentWord,number);    
       }
       number++;
     }
     idalbum++;
   }
-  // printf("panjang album : %d\n",panjg);
 }
 
 // int main(){
