13186559d7dc5ad05a43c6940e315480853049d8
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2023-11-20 14:10:38 +0700

    listb.h bair ga tabrakan ges

--- Source/ADT/ADT_LinkedList/listb.h
+++ Source/ADT/ADT_LinkedList/listb.h
@@ -30,11 +30,11 @@ boolean IsEmptylistb(listBerkait L);
 /* Mengirim true jika list kosong */
 
 /****************** PEMBUATAN LIST KOSONG ******************/
-void CreateEmpty(listBerkait *L);
+void CreateEmptylistb(listBerkait *L);
 /* I.S. sembarang */
 /* F.S. Terbentuk list kosong */
 
-void Dealokasi(address *P);
+void Dealokasilistb(address *P);
 /* Melakukan dealokasi/pengembalian address P */
 
 /****************** PENCARIAN SEBUAH ELEMEN LIST ******************/
@@ -45,22 +45,22 @@ address Searchlistb (listBerkait L, char *kata) ;
 
 /****************** PRIMITIF BERDASARKAN NILAI ******************/
 /*** PENAMBAHAN ELEMEN ***/
-void InsVFirst (listBerkait *L,char *penyanyi,  char *album,  char *lagu );
+void InsVFirstlistb (listBerkait *L,char *penyanyi,  char *album,  char *lagu );
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen pertama dengan nilai X jika alokasi berhasil */
-void InsVLast (listBerkait *L, char *penyanyi,  char *album,  char *lagu) ;
+void InsVLastlistb (listBerkait *L, char *penyanyi,  char *album,  char *lagu) ;
 /* I.S. L mungkin kosong */
 /* F.S. Melakukan alokasi sebuah elemen dan */
 /* menambahkan elemen list di akhir: elemen terakhir yang baru */
 /* bernilai X jika alokasi berhasil. Jika alokasi gagal: I.S. = F.S. */
 
 /*** PENGHAPUSAN ELEMEN ***/
-void DelVFirst(listBerkait *L, Isi_Que *X);
+void DelVFirstlistb(listBerkait *L, Isi_Que *X);
 /* I.S. List L tidak kosong */
 /* F.S. Elemen pertama list dihapus: nilai info disimpan pada X */
 /* dan alamat elemen pertama di-dealokasi */
-void DelVLast(listBerkait *L, Isi_Que *X);
+void DelVLastlistb(listBerkait *L, Isi_Que *X);
 /* I.S. list tidak kosong */
 /* F.S. Elemen terakhir list dihapus: nilai info disimpan pada X */
 /* dan alamat elemen terakhir di-dealokasi */
@@ -70,7 +70,7 @@ void DelVLast(listBerkait *L, Isi_Que *X);
 void InsertFirstlistb(listBerkait *L, address P);
 /* I.S. Sembarang, P sudah dialokasi */
 /* F.S. Menambahkan elemen ber-address P sebagai elemen pertama */
-void InsertAfter(listBerkait *L, address P, address Prec);
+void InsertAfterlistb(listBerkait *L, address P, address Prec);
 /* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
 /* P sudah dialokasi */
 /* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
@@ -79,47 +79,47 @@ void InsertLastlistb(listBerkait *L, address P);
 /* F.S. P ditambahkan sebagai elemen terakhir yang baru */
 
 /*** PENGHAPUSAN SEBUAH ELEMEN ***/
-void DelFirst(listBerkait *L, address *P);
+void DelFirstlistb(listBerkait *L, address *P);
 /* I.S. List tidak kosong */
 /* F.S. P adalah alamat elemen pertama list sebelum penghapusan */
 /* Elemen list berkurang satu (mungkin menjadi kosong) */
 /* First element yg baru adalah suksesor elemen pertama yang lama */
-void DelP (listBerkait *L, char *lagu);
+void DelPlistb (listBerkait *L, char *lagu);
 /* I.S. Sembarang */
 /* F.S. Jika ada elemen list ber-address P, dengan info(P) = X */
 /* Maka P dihapus dari list dan di-dealokasi */
 /* Jika tidak ada elemen list dengan info(P) = X, maka list tetap */
 /* List mungkin menjadi kosong karena penghapusan */
-void DelLast(listBerkait *L, address *P);
+void DelLastlistb(listBerkait *L, address *P);
 /* I.S. List tidak kosong */
 /* F.S. P adalah alamat elemen terakhir list sebelum penghapusan */
 /* Elemen list berkurang satu (mungkin menjadi kosong) */
 /* Last element baru adalah predesesor elemen terakhir yg lama, */
 /* jika ada */
-void DelAfter(listBerkait *L, address *Pdel, address Prec);
+void DelAfterlistb(listBerkait *L, address *Pdel, address Prec);
 /* I.S. List tidak kosong. Prec adalah anggota list */
 /* F.S. Menghapus Next(Prec): */
 /* Pdel adalah alamat elemen list yang dihapus */
 
 /****************** PROSES SEMUA ELEMEN LIST ******************/
-void PrintInfo(listBerkait L);
+void PrintInfolistb(listBerkait L);
 /* I.S. List mungkin kosong */
 /* F.S. Jika list tidak kosong, ia list dicetak ke kanan: [e1,e2,...,en] */
 /* Contoh: jika ada tiga elemen bernilai "one", "two", "three" akan dicetak: ["one","two","three"] */
 /* Jika list kosong: menulis [] */
 /* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
-int NbElmt(listBerkait L);
+int NbElmtlistb(listBerkait L);
 /* Mengirimkan banyaknya elemen list; mengirimkan 0 jika list kosong */
 
 /****************** PROSES TERHADAP LIST ******************/
 
-void InversList(listBerkait *L);
+void InversListlistb(listBerkait *L);
 /* I.S. sembarang. */
 /* F.S. elemen list dibalik: */
 /* Elemen terakhir menjadi elemen pertama, dan seterusnya. */
 /* Membalik elemen list, tanpa melakukan alokasi/dealokasi. */
 
-void Konkat1(listBerkait *L1, listBerkait *L2, listBerkait *L3);
+void Konkat1listb(listBerkait *L1, listBerkait *L2, listBerkait *L3);
 /* I.S. L1 dan L2 sembarang */
 /* F.S. L1 dan L2 kosong, L3 adalah hasil konkatenasi L1 & L2 */
 /* Konkatenasi dua buah list: L1 dan L2 */
@@ -127,6 +127,6 @@ void Konkat1(listBerkait *L1, listBerkait *L2, listBerkait *L3);
 
 /* Tidak ada alokasi/dealokasi pada prosedur ini */
 
-boolean IsMember(listBerkait S, char* Elmt);
+boolean IsMemberlistb(listBerkait S, char* Elmt);
 
 #endif
