63daf799f9cd4a3ae733126c8ad749b1964e36e8
steven-adrnn <stevenadrian37@gmail.com>
2023-11-21 02:37:59 +0700

    commit playsong

---
+++ Source/command/playsong.c
@@ -0,0 +1,37 @@
+#include "playsong.h"
+
+void playsong (ListPenyanyi lp, SetLagu sl, MapAlbum ma) {
+    printf("Daftar Penyanyi :\n");
+    DisplayListPenyanyi(lp);
+
+    printf("Masukkan Nama Penyanyi yang dipilih : \n");
+    STARTCOMMAND();
+    Word namapenyanyi = currentCommand;
+    int idpenyanyi = albumtoidpenyanyi(lp, namapenyanyi);
+    printf("Daftar album oleh ");
+    displayWord(namapenyanyi);
+    printf(" : \n");
+    displayMapAlbum(ma, idpenyanyi);
+
+    printf("Masukkan Nama Album yang dipilih : \n");
+    STARTCOMMAND();
+    Word namaalbum = currentCommand;
+    int idalbum = laguAlbumID(namaalbum, ma);
+    printf("Daftar lagu album ");
+    displayWord(namaalbum);
+    printf(" oleh ");
+    displayWord(namapenyanyi);
+    printf(" : \n");
+    DisplaySetLagu(sl, idalbum);
+
+    printf("Masukkan ID Lagu yang dipilih :  ");
+    STARTCOMMAND();
+    int idlagu = wordToInt(currentCommand);
+    Word namalagu = namalagufromalbum (sl, idalbum, idlagu);
+
+
+    printf ("Memutar lagu ");
+    displayWord(namalagu);
+    printf (" oleh ");
+    displayWord(namapenyanyi);
+}
---
+++ Source/command/playsong.h
@@ -0,0 +1,16 @@
+#ifndef PLAYSONG_H
+#define PLAYSONG_H
+
+#include <stdio.h>
+#include "../ADT/struc.h"
+#include "../ADT/ADT_List/liststatis.h"
+#include "../ADT/ADT_List/listdinamis.h"
+#include "../ADT/ADT_Mesin-Kata/mesinkata.h"
+#include "../ADT/ADT_MesinKarakter/mesinkarakter.h"
+#include "../ADT/boolean.h"
+#include "../ADT/ADT_Set/set.h"
+#include "../ADT/ADT_Map/map.h"
+
+void playsong(ListPenyanyi lp, SetLagu sl, MapAlbum ma);
+
+#endif
