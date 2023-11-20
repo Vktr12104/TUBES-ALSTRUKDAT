1be29a9feaecc8ec3495b83e4d66c2b778046abb
Vktr12104 <116500975+Vktr12104@users.noreply.github.com>
2023-11-20 15:10:30 +0700

    Makasih corne

--- Source/command/playlist.h
+++ Source/command/playlist.h
@@ -1,19 +1,10 @@
 #ifndef ADTPLAYLIST
 #define ADTPLAYLIST
 
-#include "../ADT/boolean.h"
-#include "../ADT/ADT_List/listdinamis.h"
-#include "../ADT/ADT_List/liststatis.h"
-#include "../ADT/ADT_Map/map.h"
-#include "../ADT/ADT_Mesin-Kata/mesinkata.h"
-#include "../ADT/ADT_MesinKarakter/mesinkarakter.h"
-#include "../ADT/ADT_Queue/circular_queue.h"
-#include "../ADT/ADT_Queue/queue.h"
-#include "../ADT/ADT_Set/set.h"
-#include "../ADT/ADT_Stack/stack.h"
 
 
 
-void CreatePlayList (ListPlaylist *input1);
+
+void CreatePlayList ();
 void playlist_add (ListPlaylist *input,ListPenyanyi lp, MapAlbum m2,SetLagu S);
 #endif
