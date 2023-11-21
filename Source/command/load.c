4efbaf4c7f96b7c644a8b55e68d03ca71b9a9d6f
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2023-11-21 12:32:17 +0700

    load

--- Source/command/load.c
+++ Source/command/load.c
@@ -10,6 +10,73 @@ boolean CekLoad(char* NamaFile){
     }
 }
 
-void Load(ListPenyanyi* Penyanyi,MapAlbum* Album, SetLagu* SetLagu, char* NamaFile, QueueLagu* QueueLagu, HistoriLagu* HistoryLagu, ListD ListPlaylist, status *LaguSekarang){
+void Load(ListPenyanyi* Penyanyi,MapAlbum* Album, SetLagu* SetLagu, char* NamaFile, QueueLagu* QueueLagu, HistoriLagu* HistoryLagu, ListDinamik ListPlaylist, status *LaguSekarang){
     STARTCOMMAND(Penyanyi,Album,SetLagu,NamaFile);
+    ADVWORD();
+    if (StrComp(wordToString(currentWord),"-")){
+        LaguSekarang->penyanyi = '-';
+        LaguSekarang->album = '-';
+        LaguSekarang->lagu = '-';
+    }
+    else{
+        LaguSekarang->penyanyi = wordToString(takewordsemicolon(currentWord, 1));
+        LaguSekarang->album = wordToString(takewordsemicolon(currentWord,2));
+        LaguSekarang->lagu = wordToString(takewordsemicolon(currentWord,3));
+    }
+
+    ADVWORD();
+    int JumlahQueueSongs = wordToInt(currentWord);
+    for (int i = 0; i<JumlahQueueSongs; i++){
+        ADVWORD();
+
+        valuetype namaP = WordToString(takewordsemicolon(currentWord, 1));
+        valuetype namaA = WordToString(takewordsemicolon(currentWord, 2));
+        valuetype namaL = WordToString(takewordsemicolon(currentWord, 3));
+
+        Cenqueue(QueueLagu, namaP, namaA, namaL);
+    }
+
+    ADVWORD();
+    HistoriLagu HistoryRAW;
+    CreateHist(&HistoryRAW);
+
+    int JumlahHistorySongs = wordToInt(currentWord);
+
+    for (int j = 0; j < JumlahHistorySongs; j++){
+        ADVWORD();
+
+        valuetype namaP = wordToString(takewordsemicolon(currentWord,1));
+        valuetype namaA = WordToString(takewordsemicolon(currentWord, 2));
+        valuetype namaL = WordToString(takewordsemicolon(currentWord, 3));
+
+        PushLagu(&HistoryRAW, namaP, namaA, namaL);
+    }
+
+    for (int j = 0; j < JumlahHistorySongs; j++){
+        valuetype namaP = wordToString(takewordsemicolon(currentWord,1));
+        valuetype namaA = WordToString(takewordsemicolon(currentWord, 2));
+        valuetype namaL = WordToString(takewordsemicolon(currentWord, 3));
+
+        PopLagu(&HistoryRAW, &namaP, &namaA, &namaL);
+        PushLagu(HistoryLagu,namaP, namaA, namaL);
+    }
+
+    ADVWORD();
+    int jumlahDinPlaylist = wordToInt(currentWord);
+
+    for (int k = 0; k<jumlahDinPlaylist; k++){
+        ADVWORD();
+
+        int jumlahLaguPlaylist = wordToInt(takeword(currentWord,1));
+        valuetype namaPlaylist = wordToString(takekata(currentWord));
+        CreatePlayList (&ListPlaylist.Content[k]);
+        for (int l = 0; l < jumlahLaguPlaylist; l++){
+            ADVWORD();
+            valuetype namaP = wordToString(takewordsemicolon(currentWord, 1));
+            valuetype namaA = WordToString(takewordsemicolon(currentWord, 2));
+            valuetype namaL = WordToString(takewordsemicolon(currentWord, 3));
+
+            InsVLastlistb(&(ListPlaylist.Content[k]), namaP, namaA, namaL);
+        }
+    }
 }
