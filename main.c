b48aabb4100e321e0092860216d130549b68deb2
Vktr12104 <116500975+Vktr12104@users.noreply.github.com>
2023-11-21 23:21:22 +0700

    perbaiki main

--- main.c
+++ main.c
@@ -25,6 +25,8 @@ int main() {
     MapAlbum m2;
     boolean start = false;
     boolean quit = false;
+    NotPlaying();
+    NotPlayingPlaylist();
     welcome();
 
     while (!quit) {
@@ -69,8 +71,6 @@ int main() {
             PLAYLISTADDSONG(listmusisi3, m2, lagu2, &DaftarPlaylist);
         } else if (StrComp(currentCommand.TabWord, "PLAYLIST ADD ALBUM")) {
             PLAYLISTADDALBUM(listmusisi3, m2, lagu2, &DaftarPlaylist);
-        } else if (StrComp2(currentCommand.TabWord, "PLAYLIST SWAP",13)) {
-            PLAYLISTSWAP(&DaftarPlaylist);
         } else if (StrComp(currentCommand.TabWord, "SONG NEXT")) {
             SongNext(&HS, &Q);
         } else if (StrComp(currentCommand.TabWord, "SONG PREVIOUS")) {
