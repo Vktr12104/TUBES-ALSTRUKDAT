64478e45a27f96ea8b3c1593a92c2e4167800ce7
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-21 12:27:09 +0700

    update makefile dan main agar bisa di run

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include "Source/ADT/ADT_MesinKarakter/mesinkarakter.h"
 #include "Source/ADT/ADT_Map/map.h"
 #include "Source/ADT/ADT_List/liststatis.h"
+#include "Source/ADT/ADT_List/listdinamis.h"
 #include "Source/command/start.h"
 #include "Source/ADT/ADT_Set/set.h"
 #include "Source/command/list.h"
+#include "Source/command/playlist.h"
 #include "Source/ADT/boolean.h"
 #include "Source/console.h"
 #include <stdio.h>
@@ -13,19 +15,21 @@ int main(){
     
     char namaFile[] = "text.txt";
     ListPenyanyi listmusisi3;
+    ListDinamik DaftarPlaylist;
     QueueLagu Q;
     CCreateQueue(&Q);
     HistoriLagu HS;
     CreateHist(&HS);
     SetLagu lagu2;
     MapAlbum m2;
-    boolean quit = false;
+    ListDinamik ld;  
     boolean start = false;
+    boolean quit = false;
     while(!quit){
         printf(">> ");
         STARTCOMMAND();
         if(StrComp(currentCommand.TabWord, "START")){
-            STARTREAD(&listmusisi3,&lagu2,&m2,namaFile);
+            STARTREAD(&listmusisi3,&lagu2,&m2,&ld,namaFile);
             start = true; // untuk menandakan sudah pernah distart
         }
         else if (StrComp(currentCommand.TabWord,"LIST DEFAULT")){
@@ -36,6 +40,8 @@ int main(){
                 printf("Command tidak bisa dieksekusi!\n");
 
             }
+        }else if(StrComp(currentCommand.TabWord,"LIST PLAYLIST")){
+            listplaylist(ld);
         }else if(StrComp(currentCommand.TabWord,"QUEUE SONG")){
             QueSong(listmusisi3,m2,lagu2,&Q);
         }else if(StrComp(currentCommand.TabWord,"QUEUE PLAYLIST")){
@@ -47,7 +53,7 @@ int main(){
         }else if(StrComp(currentCommand.TabWord,"QUEUE CLEAR")){
             QueClear(&Q);
         }else if(StrComp(currentCommand.TabWord,"PLAYLIST CREATE")){
-
+                CreatePlayList(&ld);
         }else if(StrComp(currentCommand.TabWord,"SONG NEXT")){
             SongNext(&HS,&Q);
         }else if(StrComp(currentCommand.TabWord,"SONG PREVIOUS")){
@@ -55,6 +61,13 @@ int main(){
         }
         else if (StrComp(currentCommand.TabWord,"SAVE")){
             save(listmusisi3,m2,lagu2);
+        }
+        else if (StrComp(currentCommand.TabWord,"QUIT")){
+            QUIT(listmusisi3,m2,lagu2);
+            quit = true;
+        }
+        else if (StrComp(currentCommand.TabWord,"HELP")){
+            help(start);
         }
     }
     //DisplayListPenyanyi(listmusisi3);
