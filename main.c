d5e9b82632c2d05e2ce0a944c6330f320067cd78
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-22 17:52:28 +0700

    update main

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include "Source/console.h"
 #include "Source/command/welcome.h"
 #include <stdio.h>
+#include "Source/command/play.h"
 status current;
 int main() {
     
@@ -32,11 +33,11 @@ int main() {
     while (!quit) {
         printf(">> ");
         STARTCOMMAND(); 
-        if (StrComp(currentCommand.TabWord, "START")) {
+        if (StrComp(currentCommand.TabWord, "START;")) {
             STARTREAD(&listmusisi3, &lagu2, &m2, &DaftarPlaylist, namaFile);
             start = true;
             quit=true;
-        } else if (StrComp(currentCommand.TabWord, "LOAD")) {
+        } else if (StrComp(currentCommand.TabWord, "LOAD;")) {
             // Tambahkan implementasi untuk perintah "LOAD" jika diperlukan
         } else {
             invalidCommand(&currentCommand);
@@ -47,50 +48,54 @@ int main() {
         printf(">> ");
         STARTCOMMAND();
 
-        if (StrComp(currentCommand.TabWord, "LIST DEFAULT")) {
+        if (StrComp(wordToString(currentCommand), "LIST DEFAULT;")) {
             if (start == true) {
                 listdefault(listmusisi3, lagu2, m2);
             } else {
                 printf("Command tidak bisa dieksekusi!\n");
             }
-        } else if (StrComp(currentCommand.TabWord, "LIST PLAYLIST")) {
+        } else if (StrComp(currentCommand.TabWord, "PLAY SONG;")) {
+            playsong(listmusisi3,lagu2,m2,&Q,&HS);
+        }
+        else if (StrComp(currentCommand.TabWord, "LIST PLAYLIST;")) {
             listplaylist(DaftarPlaylist);
-        } else if (StrComp(currentCommand.TabWord, "QUEUE SONG")) {
+        } else if (StrComp(currentCommand.TabWord, "QUEUE SONG;")) {
             QueSong(listmusisi3, m2, lagu2, &Q);
-        } else if (StrComp(currentCommand.TabWord, "QUEUE PLAYLIST")) {
+        } else if (StrComp(currentCommand.TabWord, "QUEUE PLAYLIST;")) {
             // ... (tambahkan implementasi untuk QUEUE PLAYLIST)
         } else if (StrComp(currentCommand.TabWord, "QUEUE SWAP")) {
+            if (currentCommand.TabWord[currentCommand.Length-1]==';'){}
             QueSwap(&Q);
         } else if (StrComp(currentCommand.TabWord, "QUEUE REMOVE")) {
             QueMove(&Q);
-        } else if (StrComp(currentCommand.TabWord, "QUEUE CLEAR")) {
+        } else if (StrComp(currentCommand.TabWord, "QUEUE CLEAR;")) {
             QueClear(&Q);
-        } else if (StrComp(currentCommand.TabWord, "PLAYLIST CREATE")) {
+        } else if (StrComp(currentCommand.TabWord, "PLAYLIST CREATE;")) {
             CreatePlayList(&DaftarPlaylist);
-        } else if (StrComp(currentCommand.TabWord, "PLAYLIST ADD SONG")) {
+        } else if (StrComp(currentCommand.TabWord, "PLAYLIST ADD SONG;")) {
             PLAYLISTADDSONG(listmusisi3, m2, lagu2, &DaftarPlaylist);
-        } else if (StrComp(currentCommand.TabWord, "PLAYLIST ADD ALBUM")) {
+        } else if (StrComp(currentCommand.TabWord, "PLAYLIST ADD ALBUM;")) {
             PLAYLISTADDALBUM(listmusisi3, m2, lagu2, &DaftarPlaylist);
         } else if (StrComp2(currentCommand.TabWord, "PLAYLIST SWAP",13)) {
             PLAYLISTSWAP(&DaftarPlaylist);
         } else if (StrComp2(currentCommand.TabWord, "PLAYLIST REMOVE",15)) {
             PLAYLISTREMOVE(&DaftarPlaylist);
         }
-        else if (StrComp(currentCommand.TabWord, "PLAYLIST DELETE")){
+        else if (StrComp(currentCommand.TabWord, "PLAYLIST DELETE;")){
             PLAYLISTDELETE(&DaftarPlaylist);
-        } else if (StrComp(currentCommand.TabWord, "SONG NEXT")) {
+        } else if (StrComp(currentCommand.TabWord, "SONG NEXT;")) {
             SongNext(&HS, &Q);
-        } else if (StrComp(currentCommand.TabWord, "SONG PREVIOUS")) {
+        } else if (StrComp(currentCommand.TabWord, "SONG PREVIOUS;")) {
             SongPrev(&HS, &Q);
         } else if (StrComp(currentCommand.TabWord, "SAVE")) {
             save(listmusisi3, m2, lagu2, Q , HS , DaftarPlaylist);
-        } else if (StrComp(currentCommand.TabWord, "QUIT")) {
+        } else if (StrComp(currentCommand.TabWord, "QUIT;")) {
             QUIT(listmusisi3, m2, lagu2, Q, HS, DaftarPlaylist);
             quit = true;
-        } else if (StrComp(currentCommand.TabWord, "HELP")) {
+        } else if (StrComp(currentCommand.TabWord, "HELP;")) {
             help(start);
         }
-        else if (StrComp(currentCommand.TabWord, "STATUS")) {
+        else if (StrComp(currentCommand.TabWord, "STATUS;")) {
             statuscurrent(Q);
         }
         else if (StrComp(currentCommand.TabWord, "CHECK")){
