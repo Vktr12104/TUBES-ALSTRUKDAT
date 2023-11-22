c7608dece738ef00b5412ef3fcc6f9dae66e474f
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-22 07:50:44 +0700

    update main

--- main.c
+++ main.c
@@ -76,6 +76,8 @@ int main() {
             PLAYLISTADDSONG(listmusisi3, m2, lagu2, &DaftarPlaylist);
         } else if (StrComp(currentCommand.TabWord, "PLAYLIST ADD ALBUM;")) {
             PLAYLISTADDALBUM(listmusisi3, m2, lagu2, &DaftarPlaylist);
+        } else if (StrComp2(currentCommand.TabWord, "PLAYLIST SWAP",13)) {
+            PLAYLISTSWAP(&DaftarPlaylist);
         } else if (StrComp2(currentCommand.TabWord, "PLAYLIST REMOVE",15)) {
             PLAYLISTREMOVE(&DaftarPlaylist);
         }
