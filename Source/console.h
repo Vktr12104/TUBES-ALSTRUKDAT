943b3d963864e57b9a94e2ac838076d210953def
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2023-11-23 13:36:04 +0700

    update console.h

--- Source/console.h
+++ Source/console.h
@@ -1,14 +1,14 @@
 
 #include "ADT/ADT_List/listdinamis.h"
 #include "ADT/ADT_List/liststatis.h"
-#include "ADT/ADT_Map/map.h"
 #include "ADT/ADT_Mesin-Kata/mesinkata.h"
-#include "ADT/ADT_MesinKarakter/mesinkarakter.h"
 #include "ADT/ADT_Queue/circular_queue.h"
-#include "ADT/ADT_Queue/queue.h"
+#include "ADT/ADT_List/liststatis.h"
+#include "ADT/ADT_Map/map.h"
 #include "ADT/ADT_Set/set.h"
+#include "ADT/ADT_LinkedList/listb.h"
+#include "ADT/ADT_List/listdinamis.h"
 #include "ADT/ADT_Stack/stack.h"
-#include "ADT/boolean.h"
 
 #ifndef CONSOLE_H
 #define CONSOLE_H
