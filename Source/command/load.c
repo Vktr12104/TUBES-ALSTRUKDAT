3e1bdf79a64abeeb51a0ec484410e57ae9e4f9a9
dedysaragih123 <dedyhofmanindosaragih@gmail.com>
2023-11-24 12:57:56 +0700

    load bismillah benar

--- Source/command/load.c
+++ Source/command/load.c
@@ -2,16 +2,16 @@
 
 boolean FileExists(char *fname) {
      FILE *file;
-     if ((file = fopen(fname, "r")))
+     file = fopen(fname, "r");
+     if ((file == NULL))
      {
           fclose(file);
-          return true;
+          return false;
      }
-     return false;
+     return true;
 }
 
 void Load(ListPenyanyi * LP, SetLagu *lagu2,MapAlbum *m2,ListDinamik *daftarplaylist, char* NamaFile, QueueLagu* QueueLagu, HistoriLagu* HistoryLagu, status *LaguSekarang){
-    
     if (FileExists(NamaFile)){
         startWFile(NamaFile);
         // inisiasi
@@ -19,74 +19,94 @@ void Load(ListPenyanyi * LP, SetLagu *lagu2,MapAlbum *m2,ListDinamik *daftarplay
         Word NamaAlbum;
         Word NamaLagu;
         int panjg;
-        CreateMapAlbum(&*m2);
+        CreateMapAlbum(m2);
         NotPlaying();
         NotPlayingPlaylist();
         *daftarplaylist = CreateLD();
-        CreateEmptySetLagu(&*lagu2);
+        CreateEmptySetLagu(lagu2);
         *LP = MakeListPenyanyi();
-        //CreateMapAlbum(&mapAl);
-        
-
-
         
         int loop = wordToInt(currentWord);
+        // printf ("%d\n",loop);
         int album,lagu;
         int number =1;
         int idalbum=1;
         for (int i = 0; i < loop; i++){
             ADVSATUKATA();
-            album = currentWord.TabWord[0] - 48;   
+            album = wordToInt(currentWord);   
             ADVKALIMAT();  
-            InsertLast(&*LP, currentWord);
+            InsertLast(LP, currentWord);
             for (int j = 0; j < album; j++){
             ADVSATUKATA();
-            lagu = currentWord.TabWord[0] - 48;
+            lagu = wordToInt(currentWord);
             ADVKALIMAT();
-            InsertMapAlbum(&*m2,i,currentWord);
+            InsertMapAlbum(m2,i,currentWord);
             for (int k = 0; k < lagu; k++)
             {
                 ADVKALIMAT();
-                InsertSetLagu(&*lagu2,currentWord,number);    
+                InsertSetLagu(lagu2,currentWord,number);    
             }
             number++;
             }
             idalbum++;
         }
-        ADVWORD();
-        displayWord(currentWord);
+        ADVKALIMAT();
         if (StrComp(wordToString(currentWord),"-")){
-        LaguSekarang->penyanyi = "-";
-        LaguSekarang->album = "-";
-        LaguSekarang->lagu = "-";
+        current.penyanyi = "-";
+        current.album = "-";
+        current.lagu = "-";
         }
         else{
-            LaguSekarang->penyanyi = wordToString(takewordsemicolon(currentWord, 1));
-            LaguSekarang->album = wordToString(takewordsemicolon(currentWord,2));
-            LaguSekarang->lagu = wordToString(takewordsemicolon(currentWord,3));
-        }                          
-        
+            current.penyanyi = wordToString(takewordsemicolon(currentWord, 1));
+            current.album = wordToString(takewordsemicolon(currentWord,2));
+            current.lagu = wordToString(takewordsemicolon(currentWord,3));
+        }
+        // printf("%s\n",current.lagu);
+        ADVKALIMAT();
+        loop = currentWord.TabWord[0]-'0';
+        // printf("%d\n", loop);
+        for (int i = 0; i < loop; i++){
+            ADVKALIMAT();
+            char* penyanyienq = wordToString(takewordsemicolon(currentWord, 1));
+            char* albumenq = wordToString(takewordsemicolon(currentWord,2));
+            char* laguenq = wordToString(takewordsemicolon(currentWord,3));
+            Cenqueue(QueueLagu,penyanyienq,albumenq,laguenq);
+        }
+        // printf("%s\n",QueueLagu->Isi[4].lagu_playlist);
+        ADVKALIMAT();
+        loop = currentWord.TabWord[0]-'0';
+        // printf("%d\n", loop);
+        for (int j = 0; j < loop; j++){
+            ADVKALIMAT();
+            char* penyanyistack = wordToString(takewordsemicolon(currentWord, 1));
+            char* albumstack = wordToString(takewordsemicolon(currentWord,2));
+            char* lagustack = wordToString(takewordsemicolon(currentWord,3));
+            PushLagu(HistoryLagu, lagustack, albumstack, penyanyistack);
+        }
+        // printf("%s\n",HistoryLagu->hist_lagu[1].lagu_playlist);
+        ADVKALIMAT();
+        loop = currentWord.TabWord[0]-'0';
+        for (int k = 0; k<loop; k++){
+            ADVWORD();
+            int sesuatu = wordToInt(currentWord);
+            ADVKALIMAT();
+            Word namaPlaylist = currentWord;
+            daftarplaylist->Content[k].Title = namaPlaylist;
+            if (sesuatu !=0){
+                for (int l = 0; l < sesuatu; l++){
+                    ADVKALIMAT();
+                    Detail value;
+                    value.Penyanyi = takewordsemicolon(currentWord, 1);
+                    value.Album = takewordsemicolon(currentWord, 2);
+                    value.Lagu = takewordsemicolon(currentWord, 3);
+                    LinkedList a = daftarplaylist->Content[k];
+                    InsertSB (&a,value,l);
+                    daftarplaylist->Content[k] = a;
+                }
+            }
+        }                   
     }
     else {
         printf("Save file tidak ditemukan. WayangWave gagal dijalankan.\n");
     }
-
-    // ADVWORD();
-    // int jumlahDinPlaylist = wordToInt(currentWord);
-
-    // for (int k = 0; k<jumlahDinPlaylist; k++){
-    //     ADVWORD();
-
-    //     int jumlahLaguPlaylist = wordToInt(takeword(currentWord,1));
-    //     char* namaPlaylist = wordToString((currentWord));
-    //     CreatePlayList (&ListPlaylist.Content[k]);
-    //     for (int l = 0; l < jumlahLaguPlaylist; l++){
-    //         ADVWORD();
-    //         char* namaP = wordToString(takewordsemicolon(currentWord, 1));
-    //         char* namaA = wordToString(takewordsemicolon(currentWord, 2));
-    //         char* namaL = wordToString(takewordsemicolon(currentWord, 3));
-
-    //         // InsVLastlistb(&(ListPlaylist.Content[k]), namaP, namaA, namaL);
-    //     }
-    // }
 }
