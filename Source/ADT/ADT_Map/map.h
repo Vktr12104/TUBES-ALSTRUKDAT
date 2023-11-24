d0675cb99540ad42e05c18ce0610945d900da7ff
rickywijaya2004@gmail.com <rickywijaya2004@gmail.com>
2023-11-24 18:13:11 +0700

    update map.h

--- Source/ADT/ADT_Map/map.h
+++ Source/ADT/ADT_Map/map.h
@@ -5,60 +5,39 @@
 #include "../struc.h"
 
 
-
-/*
-typedef struct {
-    Title nama_album[MaxEl]; // Maksimal Panjang judul album 100 char
-    SetLagu album_lagu; // Banyak Lagu dalam album sebanyak 20 
-    int lagu_length;
-    int penyanyi_id;
-} MapAlbum;
-*/
-
-/* Definisi Map M kosong : M.Count = Nil */
-/* M.Count = jumlah element Map */
-/* M.Elements = tempat penyimpanan element Map */
-
-
-/* Membuat Lagu kosong (P.album_length = Nil) sebagai Map dari Album */
+/* Membuat Lagu kosong (P.count = 0) sebagai Map dari Album */
 void CreateMapAlbum(MapAlbum *P);
 
 
-/* Mengirim true jika album Penyanyi P kosong (P.album_length = Nil) */
+/* Mengirim true jika album Penyanyi P kosong (P.count = 0) */
 boolean IsEmptyMapAlbum(MapAlbum P);
 
 
-/* Mengirim true jika album Penyanyi P penuh (P.album_length = MaxEl) */
-//boolean IsFullMapAlbum(Penyanyi P);
-
-/* Mengembalikan true jika Key K adalah member dari Penyanyi P */
+/* Mengembalikan true jika Word AT adalah member dari MapAlbum P */
 boolean IsMemberMapAlbum(MapAlbum P, Word AT);
 
 /* ### Operator Dasar Map ### */
 
 
 
-
-
-/* Mengembalikan nilai value Album dengan Key K dari Penyanyi P */
-//MapAlbum ValueMapAlbum(Penyanyi P, Key K);
-
-
-/* Menghapus array of character (Title) AT sebagai value elemen Penyanyi P dengan Key K. */
-//void DeleteMapAlbum(Penyanyi *P, Key K);
-
 void InsertMapAlbum(MapAlbum *P,valuetype idpenyanyi,Word AT);
+/* Menginsert Word AT sebagai nama_album dan penyanyi_id = idpenyanyi*/
 
 
 //int IndexOfAlbum(Penyanyi P, MapAlbum A);
 
 void displayMapAlbum(MapAlbum P,int idpenyanyi) ;
+/* Mendisplay map album*/
 
 int albumtoidpenyanyi(ListPenyanyi P,Word nama);
+/* mengirimkan id penyanyi dari sebuah masukan nama penyanyi*/
 
 int laguAlbumID(Word input,MapAlbum P);
 
+
+
 int jumlahalbumperpenyanyi(MapAlbum P,int idpenyanyi);
+/* mengirimkan banyaknya album yang dimiliki oleh penyanyi dengan id = idpenyanyi*/
 
 
 #endif
